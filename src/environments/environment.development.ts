88670f70e377ac15268a8c0795a382523794db5b
Pablo Conejos <pconejos03@gmail.com>
2024-05-07 23:26:27 +0200

    delete environment.prod

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  SPOTIFYCLIENTID: '16c30af555714db1997a059eca365a3b',
-  SPOTIFYCLIENTSECRET:'03d021c3adee480197ef438852209ccf',
-  REDIRECTURI: 'http://localhost:4200/callback'
-};
